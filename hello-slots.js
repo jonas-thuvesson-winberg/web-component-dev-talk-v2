235668dd36e4576f9089b8c9ea04dd56956e0b83
Jonas Thuvesson Winberg <jonasthuvesson@gmail.com>
2022-01-05 23:24:27 +0100

    better examples + some presentation stuff. Also stop using eval and let the parent component handle an event instead

--- hello-slots.js
+++ hello-slots.js
@@ -1,22 +1,14 @@
 componentTag = "jtw-hello-slots"
 
 customElements.define(
-  componentTag,
-  class HelloSlots extends HTMLElement {
+  "sb-hello-world",
+  class HelloWorld extends HTMLElement {
     #shadowRoot;
     constructor() {
       super();
       this.#shadowRoot = this.attachShadow({ mode: "open" });
-      const t = document.getElementById("hello-slots-template");
+      const t = document.getElementById("hello-world-template");
       this.#shadowRoot.appendChild(t.content.cloneNode(true)); // deep clone
-    }
-
-    connectedCallback() {
-      console.log(`${componentTag} connected! :)`);
-    }
-
-    disconnectedCallback() {
-      console.log(`${componentTag} disconnetcted! :(`);
     }
   }
 );
